4ddc9d2445b755d1e1a74738b5f3c47c09596879
drcdh <danielrhunter@gmail.com>
2024-06-23 01:56:19 -0400

    Things move on a grid

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rand::prelude::random;
+use rand::{thread_rng, Rng};
 
 use bevy::{
     math::bounding::{
@@ -7,14 +7,16 @@ use bevy::{
         IntersectsVolume,
     },
     prelude::*,
-    sprite::MaterialMesh2dBundle,
+    window::WindowResolution,
 };
+
+const ARENA_HEIGHT: i32 = 20;
+const ARENA_WIDTH: i32 = 20;
 
 const SCOREBOARD_FONT_SIZE: f32 = 40.0;
 const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
 
 const MONGOOSE_SIZE: Vec2 = Vec2::new(20.0, 20.0);
-const MONGOOSE_SPEED: f32 = 200.0;
 
 const BERRY_DIAMETER: f32 = 15.0;
 
@@ -23,6 +25,15 @@ const MONGOOSE_COLOR: Color = Color::rgb(0.8, 0.6, 0.0);
 const BERRY_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
 const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
 const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct MovementTimer(Timer);
 
 #[derive(Component)]
 struct MongooseHead;
@@ -62,10 +73,10 @@ struct ScoreboardUI;
 struct BerrySpawnTimer(Timer);
 
 fn spawn_mongoose(commands: &mut Commands) {
+    let (x, y) = (0, 0);
     commands.spawn((
         SpriteBundle {
             transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 0.0),
                 scale: MONGOOSE_SIZE.extend(1.0),
                 ..default()
             },
@@ -77,7 +88,8 @@ fn spawn_mongoose(commands: &mut Commands) {
         },
         MongooseHead,
         Collider,
-        Velocity(Vec2::new(0.0, 0.0)),
+        Position { x, y },
+        MovementTimer(Timer::from_seconds(0.2, TimerMode::Repeating)),
     ));
 }
 
@@ -111,26 +123,28 @@ fn spawn_scoreboard(commands: &mut Commands) {
 
 fn spawn_berry(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     time: Res<Time>,
     mut timer: ResMut<BerrySpawnTimer>,
 ) {
     if timer.0.tick(time.delta()).just_finished() {
+        let mut rng = thread_rng();
+        let x = rng.gen_range(0..ARENA_WIDTH);
+        let y = rng.gen_range(0..ARENA_HEIGHT);
         commands.spawn((
-            MaterialMesh2dBundle {
-                mesh: meshes.add(Circle::default()).into(),
-                material: materials.add(BERRY_COLOR),
-                transform: Transform::from_translation(Vec3::new(
-                    random::<f32>()*200.0-100.0,
-                    random::<f32>()*200.0-100.0,
-                    1.0
-                ))
-                    .with_scale(Vec2::splat(BERRY_DIAMETER).extend(1.0)),
+            SpriteBundle {
+                transform: Transform {
+                    scale: Vec2::splat(BERRY_DIAMETER).extend(1.0),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: BERRY_COLOR,
+                    ..default()
+                },
                 ..default()
             },
             Berry,
             Collider,
+            Position { x, y }
         ));
     }
 }
@@ -144,15 +158,15 @@ fn spawn_snake(
     let texture_atlas_layout = texture_atlas_layouts.add(
         TextureAtlasLayout::from_grid(Vec2::splat(40.0), 3, 1, None, None)
     );
-    let x = 100.0;//random::<f32>()*300.0-150.0;
-    let y = 100.0;//random::<f32>()*300.0-150.0;
+    let x = 5;
+    let y = 5;
     let snake = commands.spawn((
         SpriteBundle::default(),
         Snake,
+        MovementTimer(Timer::from_seconds(1.0, TimerMode::Repeating)),
     )).id();
     let head = commands.spawn((
         SpriteBundle {
-            transform: Transform::from_xyz(x, y, 0.0),
             texture: texture.clone(),
             ..default()
         },
@@ -161,13 +175,13 @@ fn spawn_snake(
             index: 0,
         },
         SnakeHead,
+        SnakeSegment,
         Collider,
-        Velocity(Vec2::new(0.0, 0.0)),
+        Position { x, y },
     )).id();
     commands.entity(snake).add_child(head);
     let body = commands.spawn((
         SpriteBundle {
-            transform: Transform::from_xyz(x+40.0, y, 0.0),
             texture: texture.clone(),
             ..default()
         },
@@ -177,12 +191,11 @@ fn spawn_snake(
         },
         SnakeSegment,
         Collider,
-        Velocity(Vec2::new(0.0, 0.0)),
+        Position { x: x+1, y },
     )).id();
     commands.entity(snake).add_child(body);
     let tail = commands.spawn((
         SpriteBundle {
-            transform: Transform::from_xyz(x+80.0, y, 0.0),
             texture: texture.clone(),
             ..default()
         },
@@ -192,7 +205,7 @@ fn spawn_snake(
         },
         SnakeSegment,
         Collider,
-        Velocity(Vec2::new(0.0, 0.0)),
+        Position { x: x+2, y },
     )).id();
     commands.entity(snake).add_child(tail);
 }
@@ -209,64 +222,71 @@ fn setup(
     spawn_snake(commands, asset_server, texture_atlas_layouts);
 }
 
-
-fn move_mongoose(
+fn mongoose_control(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut Transform, With<MongooseHead>>,
+    mut position_query: Query<&mut Position, With<MongooseHead>>,
+    mut timer: Query<&mut MovementTimer, With<MongooseHead>>,
     time: Res<Time>,
 ) {
-    let mut mongoose_transform = query.single_mut();
-    let mut x_direction = 0.0;
-    let mut y_direction = 0.0;
+    let mut timer = timer.get_single_mut().unwrap();
+    if timer.0.tick(time.delta()).just_finished() {
+        let mut position = position_query.single_mut();
 
-    if keyboard_input.pressed(KeyCode::ArrowLeft) {
-        x_direction -= 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::ArrowRight) {
-        x_direction += 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::ArrowUp) {
-        y_direction += 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::ArrowDown) {
-        y_direction -= 1.0;
-    }
+        let mut delta_x = 0;
+        let mut delta_y = 0;
 
-    let new_mongoose_x_position =
-        mongoose_transform.translation.x + x_direction*MONGOOSE_SPEED*time.delta_seconds();
-    let new_mongoose_y_position =
-        mongoose_transform.translation.y + y_direction*MONGOOSE_SPEED*time.delta_seconds();
-
-    // TODO bounding
-    mongoose_transform.translation.x = new_mongoose_x_position;
-    mongoose_transform.translation.y = new_mongoose_y_position;
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            delta_x -= 1;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            delta_x += 1;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) {
+            delta_y += 1;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) {
+            delta_y -= 1;
+        }
+        position.x += delta_x;
+        position.y += delta_y;
+    }
 }
 
 fn move_snakes(
-    mut snakes_query: Query<(Entity, &Children), With<Snake>>,
-    mut transforms_query: Query<&mut Transform, With<Sprite>>,
+    mut snakes_query: Query<(Entity, &Children, &mut MovementTimer), With<Snake>>,
+    mut positions_query: Query<&mut Position, With<SnakeSegment>>,
     time: Res<Time>,
 ) {
-    let delta_x = -100.0*time.delta_seconds(); // FIXME
-    let delta_y = 0.0 as f32;
-    for (_, segments_entities) in &mut snakes_query {
-        let head = segments_entities.get(0).unwrap();
-        let mut head_transform = transforms_query.get_mut(*head).unwrap();
-        head_transform.translation.x += delta_x;
-        head_transform.translation.y += delta_y;
-        // Check for turns and swap deltas as needed
-        for segment in segments_entities.get(1..).unwrap().iter() {
-            let mut segment_transform = transforms_query.get_mut(*segment).unwrap();
-            segment_transform.translation.x += delta_x;
-            segment_transform.translation.y += delta_y;
+    for (_, segments_entities, mut timer) in &mut snakes_query {
+        if timer.0.tick(time.delta()).just_finished() {
+            let delta_x = -1; // FIXME
+            let delta_y = 0;
+            let head = segments_entities.get(0).unwrap();
+            let mut head_position = positions_query.get_mut(*head).unwrap();
+            head_position.x += delta_x;
+            head_position.y += delta_y;
+            // Check for turns and swap deltas as needed
+            for segment in segments_entities.get(1..).unwrap().iter() {
+                let mut segment_position = positions_query.get_mut(*segment).unwrap();
+                segment_position.x += delta_x;
+                segment_position.y += delta_y;
+            }
         }
     }
 }
 
-fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
-    for (mut transform, velocity) in &mut query {
-        transform.translation.x += velocity.x * time.delta_seconds();
-        transform.translation.y += velocity.y * time.delta_seconds();
+fn transformation(window: Query<&Window>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+    let window = window.single();
+    for (pos, mut transform) in &mut q {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        );
     }
 }
 
@@ -315,7 +335,14 @@ fn update_scoreboard(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text, Wi
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Mongoose!".into(),
+                resolution: WindowResolution::new(800., 800.).with_scale_factor_override(1.0),
+                ..default()
+            }),
+            ..default()
+        }))
         .insert_resource(Scoreboard { score: 0 })
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(BerrySpawnTimer(Timer::from_seconds(3.0, TimerMode::Repeating)))
@@ -326,11 +353,11 @@ fn main() {
         .add_systems(
             FixedUpdate,
             (
-                apply_velocity,
-                move_mongoose,
+                mongoose_control,
                 move_snakes,
-                check_for_collisions,
                 spawn_berry,
+                transformation,
+                check_for_collisions,
             ).chain(),
         )
         .add_systems(
